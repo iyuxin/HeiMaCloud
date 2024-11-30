245e5cb5da33305c315da8a621697b5632e7e4ee
Clive <1262589783@qq.com>
2024-11-30 16:25:44 +0800

    MybatisPlus条件构造器

--- src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,11 +1,17 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootTest
@@ -25,16 +31,20 @@ class UserMapperTest {
         user.setInfo("{\"age\": 24, \"intro\": \"英文老师\", \"gender\": \"female\"}");
         user.setCreateTime(LocalDateTime.now());
         user.setUpdateTime(LocalDateTime.now());
-        userMapper.saveUser(user);
+        userMapper.insert(user);
     }
 
     @Test
     void testSelectById() {
-        User user = userMapper.queryUserById(5L);
+        User user = userMapper.selectById(5L);
         System.out.println("user = " + user);
     }
 
-
+    @Test
+    void testSelectBatchIds() {
+        List<User> users = userMapper.selectBatchIds(Collections.unmodifiableList(Arrays.asList(1L, 2L, 3L)));
+        System.out.println("users = " + users);
+    }
 
 
     @Test
@@ -42,11 +52,53 @@ class UserMapperTest {
         User user = new User();
         user.setId(5L);
         user.setBalance(20000);
-        userMapper.updateUser(user);
+        userMapper.updateById(user);
     }
 
     @Test
     void testDeleteUser() {
-        userMapper.deleteUser(5L);
+        userMapper.deleteById(5L);
+    }
+
+    @Test
+    void testQueryWrapper() {
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .select("id","username", "info","balance")
+                .like("username", "o")
+                .ge("balance", 1000);
+
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    void testUpdateByQueryWrapper() {
+        User user = new User();
+        user.setBalance(2000);
+
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .eq("username", "jack");
+
+        userMapper.update(user, wrapper);
+    }
+
+    @Test
+    void testUpdateWrapper(){
+        List<Long> ids = Collections.unmodifiableList(Arrays.asList(1L, 2L, 4L));
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>()
+                .setSql("balance = balance - 200")
+                .in("id", ids);
+        userMapper.update(null, wrapper);
+    }
+
+    @Test
+    void testLambdaWrapper(){
+
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>()
+                .select(User::getId,User::getUsername,User::getInfo,User::getBalance)
+                .like(User::getUsername, "o")
+                .ge(User::getBalance, 1000);
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
     }
 }
